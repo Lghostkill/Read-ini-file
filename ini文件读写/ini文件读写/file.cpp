f989c4e8cf71020f68df622e11f62045996a9dcd
LI Guangxu <1452678195@qq.com>
2015-10-31 19:12:33 +0800

    实现动态申请

--- ini文件读写/ini文件读写/file.cpp
+++ ini文件读写/ini文件读写/file.cpp
@@ -23,6 +23,7 @@ File::~File()
 */
 void File::getFileString()
 {
+	_Value = new char[10000];
 	GetPrivateProfileString(
 		(LPCSTR)_Section,
 		(LPCSTR)_Key,
@@ -53,8 +54,9 @@ void File::writeFileString()
 *  \pram 传入源数组
 */
 
-void File::inputFileName(char* c)
+void File::inputFileName(char* c, int len)
 {
+	_FileName = new char[sizeof(_FileName)*(len + 1)];
 	strcpy(_FileName, c);
 }
 
@@ -64,8 +66,9 @@ void File::inputFileName(char* c)
 *  \pram 传入源数组
 */
 
-void File::inputSection(char* c)
+void File::inputSection(char* c, int len)
 {
+	_Section = new char[sizeof(_Section)*(len + 1)];
 	strcpy(_Section, c);
 }
 
@@ -75,8 +78,9 @@ void File::inputSection(char* c)
 *  \pram 传入源数组
 */
 
-void File::inputKey(char* c)
+void File::inputKey(char* c, int len)
 {
+	_Key = new char[sizeof(_Key)*(len + 1)];
 	strcpy(_Key, c);
 }
 
@@ -86,8 +90,9 @@ void File::inputKey(char* c)
 *  \pram 传入源数组
 */
 
-void File::inputValue(char* c)
+void File::inputValue(char* c, int len)
 {
+	_Value = new char[sizeof(_Value)*(len + 1)];
 	strcpy(_Value, c);
 }
 
@@ -97,7 +102,34 @@ void File::inputValue(char* c)
 *  \pram 传入源数组
 */
 
-void File::outputValue(char* c)
+void File::outputValue(char* c, int len)
 {
 	strcpy(c, _Value);
+}
+
+
+/*
+* \brief释放内存
+*/
+void File::intdeleted()
+{
+	delete[]_Section;
+	delete[]_Key;
+	delete[]_Value;
+
+}
+
+
+/*
+* \brief释放内存
+*/
+void File::outdeleted()
+{
+	delete[]_Section;
+	delete[]_Key;
+}
+
+void File::namedeleted()
+{
+	delete[]_FileName;
 }
--- ini文件读写/ini文件读写/main.cpp
+++ ini文件读写/ini文件读写/main.cpp
@@ -1,17 +1,19 @@
 #include<Windows.h>
 #include<iostream>
+#include<cstring>
 #include"file.h"
 
 int main()
 {
 
 	File file;
-	int choice;
+	int choice,len;
 	char c;
-	char temp[100];   
+	char temp[10000];   
 	std::cout << "请输入.ini文件地址" << std::endl << "例如：e://example.ini" << std::endl;
 	std::cin >> temp;
-	file.inputFileName(temp);
+	len = strlen(temp);
+	file.inputFileName(temp,len);
 	getchar();
 	while (1)
 	{
@@ -22,32 +24,44 @@ int main()
 		{
 			std::cout << "请输入希望写入的Section" << std::endl;
 			gets(temp);
-			file.inputSection(temp);
+			len = strlen(temp);
+			file.inputSection(temp, len);
 			std::cout << "请输入希望写入的Key" << std::endl;
 			gets(temp);
-			file.inputKey(temp);
+			len = strlen(temp);
+			file.inputKey(temp, len);
 			std::cout << "请输入希望写入的Value" << std::endl;
 			gets(temp);
-			file.inputValue(temp);
+			len = strlen(temp);
+			file.inputValue(temp, len);
 			file.writeFileString();
 			std::cout << "Finished" << std::endl;
+			file.intdeleted();
 		}
 		if (2 == choice)
 		{
 			std::cout << "请输入希望查询的Section" << std::endl;
 			gets(temp);
-			file.inputSection(temp);
+			len = strlen(temp);
+			file.inputSection(temp, len);
 			std::cout << "请输入希望查询的Key" << std::endl;
 			gets(temp);
-			file.inputKey(temp);
+			len = strlen(temp);
+			file.inputKey(temp, len);
 
 			file.getFileString();
-			file.outputValue(temp);
+			file.outputValue(temp, len);
 			std::cout << temp << std::endl;
 			std::cout << "Finished" << std::endl;
+			file.outdeleted();
 		}
 		if (0 == choice)
+		{
+			file.namedeleted();
 			return 0;
+		}
+			
+
 	}
 	system("pause");
 }
