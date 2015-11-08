cfb37aa5e58981225531c0d7c8ce923dd77bfda6
RainingSun <397381299@qq.com>
2015-11-08 15:15:48 +0800

    Read-ini-file-v3.0
    增加转换格式函数

--- ini文件读写/ini文件读写/main.cpp
+++ ini文件读写/ini文件读写/main.cpp
@@ -7,13 +7,14 @@ int main()
 {
 
 	File file;
-	int choice,len;
+	int i, isvalid;
+	int choice, secondchoice, len;
 	char c;
-	char temp[10000];   
+	char temp[10000];
 	std::cout << "请输入.ini文件地址" << std::endl << "例如：e://example.ini" << std::endl;
 	std::cin >> temp;
 	len = strlen(temp);
-	file.inputFileName(temp,len);
+	file.inputFileName(temp, len);
 	getchar();
 	while (1)
 	{
@@ -22,46 +23,78 @@ int main()
 		getchar();
 		if (1 == choice)
 		{
-			std::cout << "请输入希望写入的Section" << std::endl;
+			std::cout << "请分行输入希望写入的Section,Key,Value" << std::endl;
 			gets(temp);
 			len = strlen(temp);
 			file.inputSection(temp, len);
-			std::cout << "请输入希望写入的Key" << std::endl;
 			gets(temp);
 			len = strlen(temp);
 			file.inputKey(temp, len);
-			std::cout << "请输入希望写入的Value" << std::endl;
 			gets(temp);
 			len = strlen(temp);
 			file.inputValue(temp, len);
 			file.writeFileString();
 			std::cout << "Finished" << std::endl;
-			file.intdeleted();
+			file.intDeleted();
 		}
 		if (2 == choice)
 		{
-			std::cout << "请输入希望查询的Section" << std::endl;
+			std::cout << "请分行输入希望查询的Section,Key" << std::endl;
 			gets(temp);
 			len = strlen(temp);
 			file.inputSection(temp, len);
-			std::cout << "请输入希望查询的Key" << std::endl;
 			gets(temp);
 			len = strlen(temp);
 			file.inputKey(temp, len);
-
 			file.getFileString();
-			file.outputValue(temp, len);
+			file.outPutValue(temp);
 			std::cout << temp << std::endl;
-			std::cout << "Finished" << std::endl;
-			file.outdeleted();
+			if (strcmp(temp, "No such Key or Section") != 0)
+			{
+				isvalid = 1;
+				std::cout << "希望将结果转换为" << std::endl << "1:整型		2:浮点型	 3:字符型" << std::endl;
+				std::cin >> secondchoice;
+				if (3 == secondchoice)
+					std::cout << temp << std::endl;
+				if (1 == secondchoice)
+				{
+					len = strlen(temp);
+					for (i = 0; i < len; i++)
+					{
+						if (temp[i] < '0' || temp[i] >'9')
+						{
+							std::cout << "不能转化为整型" << std::endl;
+							isvalid = 0;
+							break;
+						}
+					}
+					if (isvalid == 1)
+						std::cout << file.fromStrToInt(temp) << std::endl;
+				}
+				if (2 == secondchoice)
+				{
+					len = strlen(temp);
+					for (i = 0; i < len; i++)
+					{
+						if (temp[i] != '.' && (temp[i] < '0' || temp[i] >'9'))
+						{
+							std::cout << "不能转化为浮点型" << std::endl;
+							isvalid = 0;
+							break;
+						}
+					}
+					if (isvalid == 1)
+						std::cout << file.fromStrToFloat(temp) << std::endl;
+				}
+			}
+			else
+				printf("No such Key or Section\n");
+			if (0 == choice)
+			{
+				file.nameDeleted();
+				return 0;
+			}
 		}
-		if (0 == choice)
-		{
-			file.namedeleted();
-			return 0;
-		}
-			
-
+		system("pause");
 	}
-	system("pause");
 }
